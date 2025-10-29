f3f4f052bc07e945292d89d935b09781af4baa55
Gabrieldanttasxl <felipegabrieldsd@gmail.com>
2025-10-29 01:05:45 -0300

    Adiciona lógica para exclusão de usuários e melhora a estrutura da tabela de listagem

--- php/listar.php
+++ php/listar.php
@@ -8,16 +8,26 @@ if ($result->num_rows > 0) {
     // Saída de dados de cada linha
     while($row = $result->fetch_assoc()){
         echo "<tr>
-           <td>{$row['id']}</td>
-           <td>{$row['nome']}</td>
-           <td>{$row['email']}</td>
-           <td>{$row['telefone']}</td>
-           <td>{$row['idade']}</td>
-           </tr>";
+                 <td>{$row['id']}</td>
+                <td>{$row['nome']}</td>
+                <td>{$row['email']}</td>
+                <td>{$row['telefone']}</td>
+                <td>{$row['idade']}</td>
+                <td class='text-center'>
+                    <a href='php/excluir.php?id={$row['id']}'
+                    class='btn btn-sm btn-danger'
+                    onclick=\"return confirm('Tem certeza que deseja excluir este usuário?');\">
+                    excluir
+                  </a>
+                </td>
+             </tr>";
     }
 } else {
-    echo"<tr></tr> colspan='5' class='text-center text
-    -muted'>Nenhum usuario cadastrado ainda.</tr>";
+    echo "<tr>
+        <td colspan='6' class='text-center text-muted py-3'>
+        Nenhum usuario cadastrado ainda.
+        </td>
+    </tr>";
 }
 
 $conn->close();
